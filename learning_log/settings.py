4d9bb4b24e83b16f087e6c5c254c8dbd6d5175d2
AnastasiaLitvinova <nimairokute6@gmail.com>
2022-01-27 23:32:42 +0500

    change

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,7 +130,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Мои настройки
-LOGIN_URL = '/users/login/'
+LOGIN_URL = 'login'
 
 # Настройки django-bootstrap3
 BOOTSTRAP3 = {
