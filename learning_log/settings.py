7ac28d4fa331e80b783c79d16ec75d295ac12dd7
AnastasiaLitvinova <nimairokute6@gmail.com>
2022-01-28 01:05:26 +0500

    change

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -153,4 +154,8 @@ if os.getcwd() == '/app':
     # Конфигурация статических ресурсов.
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+        )
+
+django_heroku.settings(locals())
