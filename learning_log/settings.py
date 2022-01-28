6ef65ff6d43c3cb6eefd4b36ddb3c4d29761caa4
AnastasiaLitvinova <nimairokute6@gmail.com>
2022-01-28 14:32:38 +0500

    set debug=false for heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -149,7 +149,9 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X-FORWARDED-PROTO', 'https')
 
     # Разрешены все заголовки хостов.
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['learning-log-2947.herokuapp.com']
+
+    DEBUG = False
 
     # Конфигурация статических ресурсов.
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
